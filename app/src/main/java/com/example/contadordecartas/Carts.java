faac87c34185c0ae8cf6bc1325071409be0f5eda
otto <44638093@iespoblenou.org>
2021-10-25 22:04:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/contadordecartas/Carts.java
@@ -0,0 +1,76 @@
+package com.example.contadordecartas;
+
+public class Carts {
+    String name;
+    int manaCost;
+    String layout;
+    String type;
+    String ratity;
+    int power;
+    int toughness;
+    String imageUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getManaCost() {
+        return manaCost;
+    }
+
+    public void setManaCost(int manaCost) {
+        this.manaCost = manaCost;
+    }
+
+    public String getLayout() {
+        return layout;
+    }
+
+    public void setLayout(String layout) {
+        this.layout = layout;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRatity() {
+        return ratity;
+    }
+
+    public void setRatity(String ratity) {
+        this.ratity = ratity;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getToughness() {
+        return toughness;
+    }
+
+    public void setToughness(int toughness) {
+        this.toughness = toughness;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ app/src/main/java/com/example/contadordecartas/FirstFragment.java
@@ -0,0 +1,80 @@
+package com.example.contadordecartas;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.contadordecartas.databinding.FragmentFirstBinding;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+    private ArrayList<String> items;
+    private ListView cartsMagic;
+    private ArrayAdapter<String> adapter;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+
+        String[] data = {
+                "Los 400 golpes",
+                "El odio",
+                "El padrino",
+                "El padrino. Parte II",
+                "Ocurrió cerca de su casa",
+                "Infiltrados",
+                "Umberto D."
+        };
+
+        items = new ArrayList<>(Arrays.asList(data));
+
+        cartsMagic = view.findViewById(R.id.cartMagic);
+
+        adapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.carts_row,
+                R.id.cartsRow,
+                items
+        );
+        cartsMagic.setAdapter(adapter);
+
+        return view;
+
+    }
+
+//    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//
+//        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavHostFragment.findNavController(FirstFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public void onDestroyView() {
+//        super.onDestroyView();
+//        binding = null;
+//    }
+
+}
