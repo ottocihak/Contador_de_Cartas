73a37d33cedb16879bbf30ef432c61462abe4756
ottocihak <71967407+ottocihak@users.noreply.github.com>
2021-11-22 13:40:03 +0100

    Update FirstFragment.java

--- app/src/main/java/com/example/contadordecartas/FirstFragment.java
+++ app/src/main/java/com/example/contadordecartas/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.contadordecartas;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,7 +11,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,10 +23,11 @@ import com.example.contadordecartas.databinding.FragmentFirstBinding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FirstFragment extends Fragment {
     private ListView cardsMagic;
-    private ArrayAdapter<Cards> adapter;
+    private CardsAdapter adapter;
 
     @Override
     public View onCreateView(
@@ -32,19 +36,28 @@ public class FirstFragment extends Fragment {
     ) {
 
         View view = inflater.inflate(R.layout.fragment_first, container, false);
-        ArrayList<Cards>items = new ArrayList<>();
 
-        adapter = new ArrayAdapter<>(
+        cardsMagic = view.findViewById(R.id.cardMagic);
+
+        ArrayList<Cards> items = new ArrayList<>();
+
+        adapter = new CardsAdapter(
                 getContext(),
                 R.layout.cards_row,
-                R.id.cardsRow,
                 items
         );
-        cardsMagic = view.findViewById(R.id.cardMagic);
+
         cardsMagic.setAdapter(adapter);
         return view;
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        refresh();
+    }
+
 
     public void onCreateOptionsMenu(@NonNull Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
@@ -67,7 +80,6 @@ public class FirstFragment extends Fragment {
         protected ArrayList<Cards> doInBackground(Void... voids) {
             MagicCardsAPI api = new MagicCardsAPI();
             ArrayList<Cards> result = api.getCards();
-//  some stuff to change
             Log.d("DEBUG", result.toString());
             return result;
         }
@@ -86,28 +98,9 @@ public class FirstFragment extends Fragment {
     }
 
 
-
     private void refresh() {
         RefreshDataTask task = new RefreshDataTask();
         task.execute();
     }
-
-//    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-//
-//        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                NavHostFragment.findNavController(FirstFragment.this)
-//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-//            }
-//        });
-//    }
-//
-//    @Override
-//    public void onDestroyView() {
-//        super.onDestroyView();
-//        binding = null;
-//    }
 
 }
