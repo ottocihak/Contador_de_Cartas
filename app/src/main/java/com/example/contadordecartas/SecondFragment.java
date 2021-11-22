995425e4cbbb01b32a5971e725a90631d2ce1eb6
otto <44638093@iespoblenou.org>
2021-11-22 22:38:52 +0100

    sixth commit

---
+++ app/src/main/java/com/example/contadordecartas/SecondFragment.java
@@ -0,0 +1,29 @@
+package com.example.contadordecartas;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.contadordecartas.databinding.FragmentSecondBinding;
+
+public class SecondFragment extends Fragment {
+
+    private FragmentSecondBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+}
